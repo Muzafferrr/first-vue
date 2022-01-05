09c6a7b83be721a79f0d85392937a826447886d6
Muzaffer Erdoğan <muzaffer45e@outlook.com>
2022-01-05 18:07:35 +0300

    custom directive added

---
+++ src/examples/list-array-operations.vue
@@ -0,0 +1,42 @@
+<template>
+
+  <div>
+    <ul>
+      <li v-for="(item,index) in items" :key="index">Product Name: {{ item.type }} Stock Quantity: {{ item.number }}
+      </li>
+    </ul>
+    <button @click="tryArray()">Add</button>
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      items: [
+        {type: 'pen', number: 33},
+        {type: 'ball', number: 344},
+        {type: 'rocket', number: 563},
+        {type: 'guitar', number: 64},
+        {type: 'table', number: 12}
+      ]
+    }
+  },
+  methods: {
+    tryArray() {
+      // this.items.push({type:'pencil', number:888})   sona eleman ekler
+      // this.items.unshift({type:'pencil', number:888})   başa eleman ekler
+      // const a = this.items.pop();   sondan eleman siler
+      // const a = this.items.shift();    baştan eleman siler
+      // const a = this.items.splice(1, 2)  verilen index aralığında elemanları siler
+      // const a = this.items.splice(1, 2, {type:'pencil', number:888}, {type:'paper', number:77})  verilen index aralığında elemanları siler ve yerine verilen elemanı ekler
+      //const a = this.items.concat({type:'pencil', number:888})  ana diziyi bozmadan yeni diziye elemanı ekler
+      // const a = [...this.items, {type:'pencil', number:888}]
+      // this.items[2] = {type:'pencil', number:888} bu kullanımı vue algılamaz kullanma
+      this.$set(this.items, 2, {type: 'pencil', number: 888})  //belli bir indexteki elemana direkt atama yapmak istiyorsak yukarıdakini değil $set() fonkstyonunu kullanmalıyız
+    }
+  }
+}
+</script>
---
+++ src/examples/mustache-array-using.vue
@@ -0,0 +1,34 @@
+<template>
+
+  <div>
+    <button @click="changeArray()">Change Array</button>
+    <p>{{ number }}</p>
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      alphabet: ['a', 'b', 'c', 'd', 'e'],
+      number: [1, 2, 3, 4, 5, 6],
+      newArray: []
+    }
+  },
+  methods: {
+    changeArray() {
+      // this.newArray = this.alphabet.map(item => item === 'c' ? 'newC' : item)
+      // this.newArray = this.alphabet.filter(item => item === 'c')
+      // this.newArray = this.number.filter(item => item % 2 === 0)
+      // this.alphabet.reverse()
+      this.number.sort(function () {
+        //return a - b    küçükten büyüğe doğru sıralar
+        //return b - a    büyükten küçüğe dpğru sıralar
+        return 0.5 - Math.random()  //random sıralar
+      })
+    }
+  }
+}
+</script>
