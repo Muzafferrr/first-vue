b7abe6c4001e62154c86b90681f99a84e129d0d9
Muzaffer Erdoğan <muzaffer45e@outlook.com>
2022-01-05 12:37:36 +0300

    v-for v-model

---
+++ src/V-For.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p v-for="(item,index) in person" :key="index">name: {{ item.name }}, age: {{ item.age }}</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      person: [
+        {name: 'david', age: 12},
+        {name: 'john', age: 25},
+        {name: 'nash', age: 44},
+        {name: 'tom', age: 23},
+      ]
+    }
+  }
+}
+</script>
--- src/V-If--V-Show.vue
+++ src/V-If--V-Show.vue
@@ -1,10 +1,24 @@
 <template>
+  <!--  <div>-->
+  <!--    computed içerisinde oluşturulan bir fonksiyon kullanılırken parantezler yazılmaz. computedlar değişken gibi davranır.-->
+  <!--    method içerisindeki fonksiyonlar kullanılırken parantezler yazılır.-->
+  <!--    v-if te her seferinde element doma eklenir ve çıkartılır.-->
+  <!--    v-show da ise element başlangıçta oluşturulur ve false olması durumunda display:none yapılır-->
+  <!--    <div v-if="tryOne === 1">this is 1</div>-->
+  <!--    <div v-else-if="tryOne === 2">this is 2</div>-->
+  <!--    <div v-else-if="tryOne === 3">this is 3</div>-->
+  <!--    <div v-else>this is 4</div>-->
+  <!--  </div>-->
+
   <div>
-    <input type="text" :placeholder="inputHolder" v-on:change="changeInputCatch()" v-if="tryIt()">
-    <input type="text" :placeholder="eventInput" v-on:change="changeInput($event)" v-else>
-    <input type="text" :maxlength="maxLength">
-    <input type="text" placeholder="input 1" v-show="see">
-    <button v-on:click="buttonClick">Button</button>
+    <div v-if="tryOne">
+      this is bigger than 0.5
+      <input type="text" placeholder="bigger input" key="1">
+    </div>
+    <div v-else>
+      this is smaller than 0.5
+      <input type="text" placeholder="smaller input" key="2">
+    </div>
   </div>
 </template>
 
@@ -13,24 +27,20 @@
 export default {
   data() {
     return {
-      inputHolder: 'try it again',
-      eventInput: 'write event value',
-      maxLength: 10,
-      see: true
+      value: 2
     }
   },
   methods: {
-    changeInput($event) {
-      console.log($event.target.value)
-    },
-    changeInputCatch() {
-      console.log('value değişti')
-    },
-    buttonClick() {
-      console.log('tıklandı')
-    },
-    tryIt() {
-      return this.see !== true;
+    // tryOne(){
+    //   return 3
+    // }
+  },
+  computed: {
+    tryOne() {
+      // return 3
+      const a = Math.random() > 0.5;
+      console.log(a)
+      return a
     }
   }
 }
