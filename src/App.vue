694f6634e38b890b496d066383a5b31df8e13225
Muzaffer Erdoğan <muzaffer45e@outlook.com>
2022-02-15 09:52:45 +0300

    dynamic switch between components

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,17 @@
 <template>
   <div>
-    <button v-for="tab in tabs" :key="tab" @click="currentTab = tab">{{ tab }}</button>
-    <!--    keep alive componentler arası geçişte verinin kalmasını sağlar-->
-    <keep-alive>
-      <component :is="currentTab"></component>
-    </keep-alive>
   </div>
 </template>
 
 <script>
-import ComponentA from "@/components/ComponentA";
-import ComponentB from "@/components/ComponentB";
 
 export default {
   data() {
     return {
-      currentTab: 'ComponentA',
-      tabs: ['ComponentA', 'ComponentB']
     }
   },
   methods: {},
   computed: {},
-  components: {
-    ComponentA,
-    ComponentB
-  }
+  components: {}
 }
 </script>
